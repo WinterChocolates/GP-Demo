a91864723b480c703cd0f6df6b6c41414a7c0270
WinterChocolates <1728841959@qq.com>
2025-02-20 19:48:27 +0800

    Edit mysql.go

--- API/storage/database/mysql.go
+++ API/storage/database/mysql.go
@@ -13,28 +13,45 @@ import (
 var DB *gorm.DB
 
 type MySQLConfig struct {
-	DSN         string
-	MaxIdleConn int
-	MaxOpenConn int
-	MaxLifetime time.Duration
+	DSN           string
+	MaxIdleConn   int
+	MaxOpenConn   int
+	MaxLifetime   time.Duration
+	MaxRetries    int
+	RetryInterval time.Duration
 }
 
 func loadMySQLConfig() MySQLConfig {
 	return MySQLConfig{
-		DSN:         viper.GetString("database.mysql.dsn"),
-		MaxIdleConn: viper.GetInt("database.mysql.max_idle_conn"),
-		MaxOpenConn: viper.GetInt("database.mysql.max_open_conn"),
-		MaxLifetime: viper.GetDuration("database.mysql.max_lifetime") * time.Second,
+		DSN:           viper.GetString("database.mysql.dsn"),
+		MaxIdleConn:   viper.GetInt("database.mysql.max_idle_conn"),
+		MaxOpenConn:   viper.GetInt("database.mysql.max_open_conn"),
+		MaxLifetime:   viper.GetDuration("database.mysql.max_lifetime") * time.Second,
+		MaxRetries:    viper.GetInt("database.mysql.max_retries"),
+		RetryInterval: viper.GetDuration("database.mysql.retry_interval") * time.Second,
 	}
 }
 
 func InitMySQL() (*gorm.DB, error) {
 	config := loadMySQLConfig()
 
-	db, err := gorm.Open(mysql.Open(config.DSN), &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-		Logger:                                   logger.Default.LogMode(logger.Info),
-	})
+	var db *gorm.DB
+	var err error
+
+	for i := 0; i <= config.MaxRetries; i++ {
+		db, err = gorm.Open(mysql.Open(config.DSN), &gorm.Config{
+			DisableForeignKeyConstraintWhenMigrating: true,
+			Logger:                                   logger.Default.LogMode(logger.Info),
+		})
+
+		if err == nil {
+			break
+		}
+
+		if i < config.MaxRetries {
+			time.Sleep(config.RetryInterval)
+		}
+	}
 
 	if err != nil {
 		return nil, fmt.Errorf("数据库连接失败: %v", err)
@@ -52,6 +69,18 @@ func InitMySQL() (*gorm.DB, error) {
 	DB = db
 
 	return db, autoMigrate(db)
+}
+
+func CheckMySQLHealth() error {
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return fmt.Errorf("获取数据库实例失败: %v", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		return fmt.Errorf("数据库连接异常: %v", err)
+	}
+	return nil
 }
 
 func autoMigrate(db *gorm.DB) error {
